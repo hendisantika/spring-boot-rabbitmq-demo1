43dd8c121cd246242da4bb486cf2f29ea0c605a5
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-01 10:38:53 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/SpringBootRabbitmqDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
